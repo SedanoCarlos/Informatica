69e38084716fa54a2cb0c93cbe67ea30e0c6ea42
usuario <usuario@W7-PC>
2019-03-29 18:17:13 +0100

    Cambios Carlos

--- src/informatica/Informatica.java
+++ src/informatica/Informatica.java
@@ -8,5 +8,11 @@ public class Informatica {
     }
     public static void nuevoMiguelAngel(){
         System.out.println("Nuevo metodo de MiguelAngel");
+        System.out.println("Cambio - Carlos 1");
+        System.out.println("Cambio - Carlos 2");
+        System.out.println("Cambio - Carlos 3");
+        System.out.println("Cambio - Carlos 4");
+        System.out.println("Cambio - Carlos 5");
+        System.out.println("Cambio - Carlos 6");
     }
 }
