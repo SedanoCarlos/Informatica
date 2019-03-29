155682f5bc29e0cea9e352403db7ddd58dcd83db
usuario <usuario@172.16.2.168>
2019-03-29 18:00:39 +0100

    1

--- src/informatica/Informatica.java
+++ src/informatica/Informatica.java
@@ -3,7 +3,7 @@ public class Informatica {
     public static void main(String[] args) {
         System.out.println("Modificacion 1");
     }
-    public static void nuevoCarlos(){
-        System.out.println("Nuevo metodo de Carlos");
+    public static void nuevoMiguelAngel(){
+        System.out.println("Nuevo metodo de MiguelAngel");
     }
 }
