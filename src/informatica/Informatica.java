8e16d3e755261273996fca7cbf7e76aec17beb55
usuario <usuario@172.16.2.168>
2019-03-29 18:17:17 +0100

    Cambios en metodo

--- src/informatica/Informatica.java
+++ src/informatica/Informatica.java
@@ -3,10 +3,15 @@ public class Informatica {
     public static void main(String[] args) {
         System.out.println("Modificacion 1");
     }
-    public static void nuevoCarlos(){
+    public static void nuevoCarlos() {
         System.out.println("Nuevo metodo de  Carlos");
     }
-    public static void nuevoMiguelAngel(){
-        System.out.println("Nuevo metodo de MiguelAngel");
+    public static void nuevoMiguelAngel() {
+        System.out.println("Cambio - Miguel Angel 1");
+        System.out.println("Cambio - Miguel Angel 2");
+        System.out.println("Cambio - Miguel Angel 3");
+        System.out.println("Cambio - Miguel Angel 4");
+        System.out.println("Cambio - Miguel Angel 5");
+        System.out.println("Cambio - Miguel Angel 6");
     }
 }
