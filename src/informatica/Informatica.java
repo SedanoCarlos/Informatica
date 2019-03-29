245932aed04a370f899aace267a129e243741c37
usuario <usuario@172.16.2.168>
2019-03-29 17:55:01 +0100

    Creacion de metodo nuevoMiguelAngel

--- src/informatica/Informatica.java
+++ src/informatica/Informatica.java
@@ -3,5 +3,8 @@ public class Informatica {
     public static void main(String[] args) {
         System.out.println("Modificacion 1");
     }
+    public static void nuevoMiguelÁngel() {
+        System.out.println("Nuevo metodo de Miguel Ángel");
+    }
     
 }
