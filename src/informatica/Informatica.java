fbf949b5113cc7909a2c896a360ea00ed7a33b25
usuario <usuario@W7-PC>
2019-04-08 20:22:39 +0200

    Insercion Funcionalidad 3

--- src/informatica/Informatica.java
+++ src/informatica/Informatica.java
@@ -15,6 +15,7 @@ public class Informatica {
         System.out.println("Cambio - Carlos 5");
         System.out.println("Cambio - Carlos 6");
         System.out.println("Funcionalidad 1 - Carlos");
+        System.out.println("Funcionalidad 3 - Carlos");
 
         System.out.println("Cambio - Miguel Angel 1");
         System.out.println("Cambio - Miguel Angel 2");
